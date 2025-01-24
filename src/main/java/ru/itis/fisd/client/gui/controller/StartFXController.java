72808f1dd4a73b6744ce839cc55e82a43df57757
Almaz <almaz.kzn2005@yandex.ru>
2025-01-24 23:30:08 +0300

    feat: add StartFXController class

---
+++ src/main/java/ru/itis/fisd/client/gui/controller/StartFXController.java
@@ -0,0 +1,24 @@
+package ru.itis.fisd.client.gui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import ru.itis.fisd.listener.ButtonActionListener;
+
+public class StartFXController {
+
+    @FXML
+    private TextField port;
+
+    @FXML
+    private Button start;
+
+    private ButtonActionListener buttonActionListener;
+
+    @FXML
+    public void initialize() {
+        buttonActionListener = new ButtonActionListener();
+        System.out.println(port);
+        start.setOnAction(_ -> buttonActionListener.handleButtonAction(start));
+    }
+}
