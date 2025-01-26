2b0f4820b060e9456fbe04e0931780bc4ee088f4
Almaz <almaz.kzn2005@yandex.ru>
2025-01-26 16:19:16 +0300

    feat: update client logic

--- src/main/java/ru/itis/fisd/client/Client.java
+++ src/main/java/ru/itis/fisd/client/Client.java
@@ -1,19 +1,37 @@
 package ru.itis.fisd.client;
 
 import javafx.application.Application;
+import lombok.Getter;
+import lombok.Setter;
 import ru.itis.fisd.client.gui.MainFX;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 
+@Getter
+@Setter
 public class Client {
+
+    private Socket socket;
+
+    public void connectToServer(String host, int port) throws IOException {
+        try {
+            socket = new Socket(host, port);
+            System.out.println("Client connected to server: " + host + ":" + port);
+
+            // Отправка тестового сообщения на сервер
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("Hello from client!".getBytes());
+            outputStream.flush();
+
+        } catch (IOException e) {
+            System.out.println("Failed to connect to server: " + e.getMessage());
+            throw e;
+        }
+    }
+
     public static void main(String[] args) {
-
-//        MainFX.setClientSocket(clientSocket);
-        //            Socket clientSocket = new Socket("localhost", 50000);
-        System.out.println("From client: connected");
-
-
         Application.launch(MainFX.class, args);
     }
 }
