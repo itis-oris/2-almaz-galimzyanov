1747f68b70510de2dd026bb59346e11955d179c3
Almaz <almaz.kzn2005@yandex.ru>
2025-01-27 14:54:15 +0300

    feat: update ProtocolType class, add fromValue method

--- src/main/java/ru/itis/fisd/protocol/ProtocolType.java
+++ src/main/java/ru/itis/fisd/protocol/ProtocolType.java
@@ -1,7 +1,24 @@
 package ru.itis.fisd.protocol;
 
+import lombok.Getter;
+
+@Getter
 public enum ProtocolType {
 
-    SYSTEM, GAME
+    PING("0"), INFO("1"), ERROR("2"), GAME("3");
 
+    private String value;
+
+    ProtocolType(String i) {
+        value = i;
+    }
+
+    public static ProtocolType fromValue(String value) {
+        for (ProtocolType type : ProtocolType.values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant with value " + value);
+    }
 }
