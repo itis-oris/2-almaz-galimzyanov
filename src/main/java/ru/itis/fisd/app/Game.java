fef5d58b553df9d261d8caec7abc5d1f6f22f9c5
Almaz <almaz.kzn2005@yandex.ru>
2025-01-27 14:57:02 +0300

    feat: big Game update

--- src/main/java/ru/itis/fisd/app/Game.java
+++ src/main/java/ru/itis/fisd/app/Game.java
@@ -1,6 +1,8 @@
 package ru.itis.fisd.app;
 
+import javafx.application.Application;
 import ru.itis.fisd.client.Client;
+import ru.itis.fisd.client.gui.MainFX;
 import ru.itis.fisd.server.Server;
 
 import java.io.IOException;
@@ -8,14 +10,19 @@ import java.io.IOException;
 public class Game {
 
     private static final int SERVER_PORT = 50000;
-    private boolean isSuccess = false;
+    private static MainFX mainFX;
 
     public Game() throws IOException {
 
+        boolean isSuccess = false;
         try {
 
             Client client = new Client();
             client.connectToServer("localhost", SERVER_PORT);
+
+            mainFX.setClientSocket(client.getSocket());
+            mainFX.todo();
+            System.out.println("SAODKASODKDO" + mainFX.getClientSocket());
             isSuccess = true;
         } catch (Exception e) {
             new Thread(() -> Server.main(new String[]{String.valueOf(SERVER_PORT)})).start();
@@ -28,9 +35,20 @@ public class Game {
             if (!isSuccess) {
                 Client client = new Client();
                 client.connectToServer("localhost", SERVER_PORT);
+
+                mainFX.setClientSocket(client.getSocket());
+                mainFX.todo();
+
+                System.out.println("SAODKASODKDO" + mainFX.getClientSocket());
             }
+
         }
 
 
+    }
+
+    public static void main(String[] args) {
+        mainFX = new MainFX();
+        Application.launch(MainFX.class, args);
     }
 }
