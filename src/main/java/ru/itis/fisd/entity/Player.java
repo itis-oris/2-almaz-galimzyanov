d30db4dcb441d30dfd3fa530df4164a2fd078b04
Almaz <almaz.kzn2005@yandex.ru>
2025-01-29 15:50:36 +0300

    feat: add list initialization

--- src/main/java/ru/itis/fisd/entity/Player.java
+++ src/main/java/ru/itis/fisd/entity/Player.java
@@ -3,12 +3,13 @@ package ru.itis.fisd.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Player {
 
-    private List<Card> playerCards;
+    private List<Card> playerCards = new ArrayList<>();
 
 }
