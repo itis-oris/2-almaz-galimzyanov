4ae5b93923aaac58456a47c2444518c58de9a639
Almaz <almaz.kzn2005@yandex.ru>
2025-01-29 15:52:55 +0300

    feat: add new features

--- src/main/java/ru/itis/fisd/client/gui/controller/GameFXController.java
+++ src/main/java/ru/itis/fisd/client/gui/controller/GameFXController.java
@@ -1,5 +1,6 @@
 package ru.itis.fisd.client.gui.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -9,15 +10,28 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import lombok.Getter;
+import ru.itis.fisd.app.Game;
 import ru.itis.fisd.entity.Card;
 import ru.itis.fisd.entity.CardColor;
 import ru.itis.fisd.entity.Deck;
+import ru.itis.fisd.entity.Player;
+import ru.itis.fisd.listener.ButtonActionListener;
+import ru.itis.fisd.listener.GameActionListener;
 
 import java.util.Random;
 
 public class GameFXController {
+
+    @Getter
+    private static GameFXController instance;
+
+    public GameFXController() {
+        instance = this;
+    }
 
     @FXML
     private HBox buttonRow; // Контейнер для строки с кнопками
@@ -28,33 +42,34 @@ public class GameFXController {
     @FXML
     private Label current;
 
+    private static GameActionListener buttonActionListener;
+
 
     @FXML
     public void initialize() {
+        buttonActionListener = new GameActionListener(this); // Передаём текущий контроллер
 
-        int totalButtons = 7; // Пример: 50 кнопок
+        int totalButtons = 7;
+        Player player = new Player();
 
-        // Set green background for the button
-        coloda.setBackground(new Background(new BackgroundFill(
-                Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-
-        // Set light blue background for the label
-        current.setBackground(new Background(new BackgroundFill(
-                Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-
+        coloda.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        current.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        current.setFont(Font.font("Arial", FontWeight.BOLD, 128));
+        current.setStyle("-fx-font-size: 400%;");
+        current.setTextFill(Color.WHITE);
+        current.setPrefWidth(80);
+        current.setPrefHeight(150);
         for (int i = 0; i < totalButtons; i++) {
-            // Создаем кнопку
-            Random rand = new Random();
-            CardColor color = CardColor.values()[rand.nextInt(CardColor.values().length)];
             Card card = getRandomCard();
-            Button button = getButton(card);
-
-            // Добавляем кнопку в строку
+            player.getPlayerCards().add(card);
+            Button button = getButton(card, player);
             buttonRow.getChildren().add(button);
         }
+
     }
 
-    private static Button getButton(Card card) {
+
+    private static Button getButton(Card card, Player player) {
         Button button = new Button(String.valueOf(card.value()));
 
         button.setFont(Font.font("Arial", FontWeight.BOLD, 128));
@@ -63,16 +78,29 @@ public class GameFXController {
         button.setStyle("-fx-font-size: 500%;");
 
         button.setPrefWidth(80);
-        button.setPrefHeight(500);
+        button.setPrefHeight(150);
         BackgroundFill background_fill = new BackgroundFill(card.color().getColor(), CornerRadii.EMPTY, Insets.EMPTY);
 
         button.setBackground(new Background(background_fill));
 
-        button.setOnAction(_ -> System.out.println("Нажата кнопка: " + button.getText()));
+        button.setOnAction(_ -> {
+            System.out.println("Нажата кнопка: " + button.getText());
+            buttonActionListener.handleButton(button, player);
+        });
         return button;
     }
 
     private Card getRandomCard() {
         return Deck.cards.remove();
+    }
+
+    public void setDeckCard(String value, Paint color) {
+
+        if (current != null) {
+            Platform.runLater(() -> {
+                current.setText(value);
+                current.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+            });
+        }
     }
 }
