e426f6566a48b0384d7c08ccc49aeb9d52b6474d
Almaz <almaz.kzn2005@yandex.ru>
2025-01-29 10:34:35 +0300

    feat: add Player entity

---
+++ src/main/java/ru/itis/fisd/entity/Player.java
@@ -0,0 +1,14 @@
+package ru.itis.fisd.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Player {
+
+    private List<Card> playerCards;
+
+}
