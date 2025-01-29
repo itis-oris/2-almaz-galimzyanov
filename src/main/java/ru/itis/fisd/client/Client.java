ba99576ba82ec9d64796b76307cd02bbcf6dc614
Almaz <almaz.kzn2005@yandex.ru>
2025-01-29 15:54:57 +0300

    feat: add correct communication with server

--- src/main/java/ru/itis/fisd/client/Client.java
+++ src/main/java/ru/itis/fisd/client/Client.java
@@ -1,10 +1,12 @@
 package ru.itis.fisd.client;
 
-import javafx.application.Application;
+import javafx.scene.paint.Paint;
 import lombok.Getter;
 import lombok.Setter;
-import ru.itis.fisd.client.gui.MainFX;
+import ru.itis.fisd.app.GameState;
+import ru.itis.fisd.client.gui.controller.GameFXController;
 import ru.itis.fisd.client.gui.controller.SceneController;
+import ru.itis.fisd.entity.Player;
 import ru.itis.fisd.protocol.Converter;
 import ru.itis.fisd.protocol.Protocol;
 import ru.itis.fisd.protocol.ProtocolType;
@@ -13,19 +15,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 
 @Getter
 @Setter
 public class Client {
 
     private Socket socket;
+    private int order;
+    private Player player;
 
     public void connectToServer(String host, int port) throws IOException {
         try {
             socket = new Socket(host, port);
             System.out.println("Client connected to server: " + host + ":" + port);
 
-            // Создаем поток для отправки пинг-сообщений
             new Thread(() -> {
                 try (OutputStream outputStream = socket.getOutputStream()) {
                     while (!socket.isClosed()) {
@@ -53,17 +57,41 @@ public class Client {
     private void handleMessages() {
         try (InputStream input = socket.getInputStream()) {
             byte[] buffer = new byte[1024];
-            while (input.read(buffer) != -1) {
-                Protocol protocol = Converter.decode(buffer);
-                String message = protocol.body();
-                System.out.println("Received from server: " + message);
+            while (!socket.isClosed()) {
+                int res = input.read(buffer);
+                if (res != -1) {
+                    Protocol protocol = Converter.decode(buffer);
+                    String message = protocol.body();
+                    System.out.println("Received from server: " + message);
 
-
-                switch (message) {
-                    case "start" -> SceneController.activate("game");
-                    case "wait" -> {
-                        System.out.println("Waiting for message...sdsfsdfsdfsdfds");
-                        SceneController.activate("waiting_room");
+                    if (message.startsWith("updateState")) {
+                        // Разбираем новое состояние, например: "updateState:order=2,isStart=true"
+                        String[] parts = message.split(":")[1].split(",");
+                        for (String part : parts) {
+                            String[] keyValue = part.split("=");
+                            switch (keyValue[0]) {
+                                case "order" -> GameState.order = Integer.parseInt(keyValue[1]);
+                                case "isStart" -> GameState.isStart = Boolean.parseBoolean(keyValue[1]);
+                            }
+                        }
+                        System.out.println("Updated GameState: order=" + GameState.order + ", isStart=" + GameState.isStart);
+                    } else if (message.startsWith("card")) {
+                        System.out.println(message);
+                        System.out.println(Arrays.toString(message.split(":")));
+                        String[] parts = message.split(":")[1].split("&");
+                        System.out.println(Arrays.toString(parts));
+                        String value = parts[0];
+                        String color = parts[1];
+                        GameFXController.getInstance().setDeckCard(value, Paint.valueOf(color));
+                    }
+                    else {
+                        switch (message) {
+                            case "start" -> SceneController.activate("game");
+                            case "wait" -> {
+                                System.out.println("Waiting for message...");
+                                SceneController.activate("waiting_room");
+                            }
+                        }
                     }
                 }
             }
@@ -72,7 +100,19 @@ public class Client {
         }
     }
 
-    public static void main(String[] args) {
-        Application.launch(MainFX.class, args);
+
+    // Новый метод для отправки сообщений на сервер
+    public void sendMessage(Protocol message) {
+        try {
+            OutputStream output = socket.getOutputStream();
+            // Кодируем сообщение в Protocol, если нужно
+            byte[] encodedMessage = Converter.encode(message);
+
+            output.write(encodedMessage);  // Отправляем данные
+            output.flush();  // Обязательно сбрасываем поток для отправки данных
+            System.out.println("Message sent to server: " + message);
+        } catch (IOException e) {
+            System.out.println("Error sending message: " + e.getMessage());
+        }
     }
 }
